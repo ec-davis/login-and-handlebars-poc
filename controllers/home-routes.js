6cfcb8031c3503a66dbda2ebc6d7b7ad5e2fc8bd
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-22 15:35:46 -0600

    adds stub login and dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,5 +11,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/dashboard", async (req, res) => {
+  try {
+    console.log("home-routes:", req.method, req.path);
+    res.render("dashboard");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    console.log("home-routes:", req.method, req.path);
+    res.render("login");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 /* EXPORTS */
 module.exports = router;
