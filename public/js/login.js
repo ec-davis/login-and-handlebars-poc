6c58341e5a5ae65d3095ce854b1c74e5e27d6a58
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-23 17:56:28 -0600

    calls login route

--- public/js/login.js
+++ public/js/login.js
@@ -2,15 +2,32 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
+
+  if (!(email && password)) {
+    alert("Please supply email and password");
+    return;
+  }
   alert("email " + email + " password " + password);
+  const response = await fetch("/api/users/login", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: { "Content-Type": "application-json" },
+  });
+
+  if (response.ok) document.location.replace("/");
+  else alert("holy cow, failed to log in");
 };
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
-  const username = document.querySelector("#username-signup").value.trim();
+  //   const username = document.querySelector("#username-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
-  alert("username " + username + " email " + email + " password " + password);
+  if (!(email && password)) {
+    alert("Please supply email and password");
+    return;
+  }
+  alert(" email " + email + " password " + password);
 };
 
 document
