1b14b23ace32a60f6eb8949745ee7f5c35fed986
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-23 17:48:47 -0600

    get login and signup field values

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,16 @@
 const loginFormHandler = async (event) => {
-  alert("login");
+  event.preventDefault();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+  alert("email " + email + " password " + password);
 };
 
 const signupFormHandler = async (event) => {
-  alert("signup");
+  event.preventDefault();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  alert("username " + username + " email " + email + " password " + password);
 };
 
 document
