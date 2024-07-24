3c8341a146161d22349aec21f838d23fc233f9b7
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-23 19:55:39 -0600

    adds post user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,23 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+// signup - creates new user
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.create({
+      emailAddress: req.body.emailAddress,
+      password: req.body.password,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(
+      "user-routes post /: Error occurred while creating User",
+      error
+    );
+    res.status(500).json(error);
+  }
+});
+
 router.post("/login", async (req, res) => {
   try {
     req.session.save(() => {
