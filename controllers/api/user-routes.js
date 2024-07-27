0e5af02c0d8b6d3e36136c16da5fd343b6b5e835
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-27 15:55:41 -0600

    protected dev-only methods

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,26 +1,30 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+// checks DEV_MODE to protect against enabling dev-only methods in production
+const isDevMode = process.env.DEV_MODE == "true" ? true : false;
+isDevMode
+  ? console.log("DEV MODE enabled. Dev-only methods are available")
+  : console.log(
+      "Running in production mode. Dev-only methods will throw errors if called"
+    );
+
 // signup - creates new user
 router.post("/", async (req, res) => {
   try {
-    console.log("user-routes post: create user");
+    console.log("user-routes", req.method, req.url);
     const user = await User.create({
       email: req.body.email,
       password: req.body.password,
     });
 
-    // req.session.save(() => {
-    //   req.session.loggedIn = true;
-    //   res.status(200).json({ user: user });
-    // });
-    console.log("I think it worked");
-    res.status(200).json(user);
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json({ user: user });
+      console.log("End of save block, req.session: ", req.session);
+    });
   } catch (error) {
-    console.log(
-      // `Error occurred while creating User: ${error.errors[0].message}`
-      `Error occurred while creating User: ${error}`
-    );
+    console.log(`Error occurred while creating User: ${error}`);
 
     res.status(500).json(`Error occurred while creating User: ${error}`);
   }
@@ -32,7 +36,7 @@ router.post("/login", async (req, res) => {
     console.log("user-routes post: login");
     console.log("req.body", req.body);
     req.session.save(() => {
-      //   req.session.loggedIn = true;
+      req.session.loggedIn = true;
       res.status(200).json("Login successful");
     });
   } catch (error) {
@@ -40,9 +44,28 @@ router.post("/login", async (req, res) => {
   }
 });
 
-// Returns all users. For development only - not to call from front end.
-// Excludes the password from the response.
+/////////////////////////////////////////////
+// DEV METHODS - for use only during development
+
+// deletes all users, to clear the clutter of test users
+router.delete("/", async (req, res) => {
+  console.log("user-routes", req.method, req.url);
+  if (!isDevMode)
+    throw new Error("Dev-only method called when in production mode");
+  try {
+    await User.destroy({ where: {} });
+    res.status(200).json("Users deleted");
+  } catch (error) {
+    console.log(`Error occurred while deleting all Users: ${error}`);
+    res.status(500).json(`Error occurred while creating User: ${error}`);
+  }
+});
+
+// Returns all users. Excludes the password from the response.
 router.get("/", async (req, res) => {
+  console.log("user-routes", req.method, req.url);
+  if (!isDevMode)
+    throw new Error("Dev-only method called when in production mode");
   try {
     const userData = await User.findAll({
       attributes: { exclude: ["password"] },
