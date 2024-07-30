c4f9152231dfab82ea542a339738a5884ab13ec3
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-30 14:20:12 -0700

    capitalize ./User

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,6 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes.js");
 
 router.use("/users", userRoutes);
-// router.post("/users/login", async (req, res) => {
-//   res.json("forks from login!");
-// });
 
 /* EXPORTS */
 module.exports = router;
